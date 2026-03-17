7fa2b1c30fd05d54657081041d21ebd09934b8a1
ivaanrodriiguezzz <ivanrodriguezgomez-landero.guadalupe@alumnado.fundacionloyola.net>
2026-03-17 23:12:07 +0100

    a

---
+++ Pruebas/EnviarAgradecimiento.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Iván Rodríguez Gómez-Landero">
+        <link rel="stylesheet" href="estilos2.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+SC:wght@100..900&display=swap" rel="stylesheet">
+        <title>Enviar Un Agradecimiento</title>
+    </head>
+    <body>
+        <header>
+            <img src="foto.png" alt="Jesuitas.png">
+        </header>
+        <nav>
+            <h1><span class="mayuscula">E</span>NVIAR UN <span class="mayuscula">A</span>GRADECIMIENTO</h1>
+            <hr id="linea1">
+            <form class="formulario">
+                <select name="" id="">
+                    <php?
+                        <option value="1"></option>
+                        <!--value seria el identificador del alumno el nombre que vaya dentro del option seria el nombre del alumno
+                        se añadiran filas de agradecimiento en la base de datos 
+                        -->
+                    ?>
+                </select>
+            </form>
+        </nav>
+    </body>
+</html>
